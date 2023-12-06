5bc76ea98d0e91120d580b6e328853a73c95d5bb
Seongwon <sngoneee@gmail.com>
2023-12-06 17:23:56 +0900

    first commit

---
+++ src/HelloJava.java
@@ -0,0 +1,10 @@
+public class HelloJava {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+//    public static void main : 프로그램의 시작점, 여기서부터 시작함
+
+
+}
